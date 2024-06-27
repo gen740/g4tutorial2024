f89d0e21cf8cc7e6996d657c25f8137dc8a831f9
Gen <54583542+gen740@users.noreply.github.com>
2024-06-27 11:31:38 +0900

    Update

--- P06_Geometry/BGO_One/source/Application_Main.cc
+++ P06_Geometry/BGO_One/source/Application_Main.cc
@@ -1,54 +1,53 @@
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Geant4 Tutorial for Hep/Space/Medicine Users
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include "FTFP_BERT.hh"
+#include "G4RunManager.hh"
+#include "G4UIExecutive.hh"
+#include "G4UImanager.hh"
+#include "G4VisExecutive.hh"
 #include "Geometry.hh"
 #include "UserActionInitialization.hh"
 
-#include "G4RunManager.hh"
-#include "G4UImanager.hh"
-#include "G4VisExecutive.hh"
-#include "G4UIExecutive.hh"
-#include "FTFP_BERT.hh"
-
 //-------------------------------------------------------------------------------
-  int main( int argc, char** argv )
+auto main(int argc, char** argv) -> int
 //-------------------------------------------------------------------------------
 {
-// Construct a run manager
-   auto runManager = new G4RunManager();
+  // Construct a run manager
+  auto runManager = new G4RunManager();
 
-// Set up mandatory user initialization: Geometry
-   runManager->SetUserInitialization( new Geometry{} );
+  // Set up mandatory user initialization: Geometry
+  runManager->SetUserInitialization(new Geometry{});
 
-// Set up mandatory user initialization: Physics-List
-   runManager->SetUserInitialization( new FTFP_BERT{} );
+  // Set up mandatory user initialization: Physics-List
+  runManager->SetUserInitialization(new FTFP_BERT{});
 
-// Set up user initialization: User Actions
-   runManager->SetUserInitialization( new UserActionInitialization{} );
+  // Set up user initialization: User Actions
+  runManager->SetUserInitialization(new UserActionInitialization{});
 
-// Initialize G4 kernel
-   runManager->Initialize();
+  // Initialize G4 kernel
+  runManager->Initialize();
 
-// Create visualization environment
-   auto visManager = new G4VisExecutive{};
-   visManager->Initialize();
+  // Create visualization environment
+  auto visManager = new G4VisExecutive{};
+  visManager->Initialize();
 
-// Get UI (User Interface) manager
-   G4UImanager* uiManager = G4UImanager::GetUIpointer();
+  // Get UI (User Interface) manager
+  G4UImanager* uiManager = G4UImanager::GetUIpointer();
 
-// Switch batch or interactive mode
-   if ( argc == 1 ) {  // Interactive mode - no command argument
-     auto uiExec = new G4UIExecutive{ argc, argv };
-     uiExec->SessionStart();
-     delete uiExec;
-   } else {            // Batch mode - 1st command argument is a macro-filename
-     G4String macroName = argv[1];
-     uiManager->ApplyCommand( "/control/execute " + macroName );
-   }
+  // Switch batch or interactive mode
+  if (argc == 1) {  // Interactive mode - no command argument
+    auto uiExec = new G4UIExecutive{argc, argv};
+    uiExec->SessionStart();
+    delete uiExec;
+  } else {  // Batch mode - 1st command argument is a macro-filename
+    G4String macroName = argv[1];
+    uiManager->ApplyCommand("/control/execute " + macroName);
+  }
 
-// Job termination
-   delete visManager;
-   delete runManager;
+  // Job termination
+  delete visManager;
+  delete runManager;
 
-   return 0;
+  return 0;
 }
